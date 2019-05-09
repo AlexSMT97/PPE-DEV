2c6b5cadcdfd2ef1ad3518a0a29e882f4eebf293
Alex <cdf.schmitt.alexandre@gmail.com>
2019-05-09 21:37:08 +0200

    Update index.php
    Titre supprimer

--- index.php
+++ index.php
@@ -7,7 +7,6 @@
     </head>
     <body>
         <header>
-            <h1>JLS</h1>
             <img src="design/logo.png" class="logo">
             <nav>
                 <a href="index.php" class="lien">Accueil</a>
