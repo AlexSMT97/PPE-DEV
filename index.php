dffb1ca489e8a1d6fbbee676dd859b3e612b5460
Alex <cdf.schmitt.alexandre@gmail.com>
2019-05-09 09:51:10 +0200

    FIrst Commit Design

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+        <title>Accueil - DevNote</title>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="design/design.css" />  
+        <link rel="shortcut icon" type="image/png" href="design/shortcut.png" />
+    </head>
+    <body>
+        <header>
+            <h1>Coucou les kneckes</h1>
+            <img src="design/logo.png" class="logo">
+            <nav>
+                <a href="index.php" class="lien">Accueil</a>
+                 - - - 
+                <a href="saisie.php" class="lien">Saisir une fiche</a>
+                 - - - 
+                <a href="recherche.php" class="lien">Rechercher</a>
+            </nav>
+        </header>
+        <audio preload="auto" autoplay loop>
+        <source src="design/Song.mp3" type="audio/mp3" />
+        Votre navigateur n'est pas compatible
+        </audio>
+        <input type="submit" value="Connexion" class="connexion" />
+    </body>
+    <footer>
+        Copyright @JLS 1990 - 2019
+    </footer>
+
+</html>
---
+++ recherche.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <title>Accueil - DevNote</title>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="design/design.css" />  
+        <link rel="shortcut icon" type="image/png" href="design/shortcut.png" />
+    </head>
+    <body>
+        <header>
+            <img src="design/logo.png" class="logo">
+            <nav>
+                <a href="index.php" class="lien">Accueil</a>
+                 - - - 
+                <a href="saisie.php" class="lien">Saisir une fiche</a>
+                 - - - 
+                <a href="recherche.php" class="lien">Rechercher</a>
+            </nav>
+        </header>
+    </body>
+    <footer>
+        Copyright @JLS 1990 - 2019
+    </footer>
+
+</html>
