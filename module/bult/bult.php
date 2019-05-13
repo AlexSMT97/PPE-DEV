434fa1f180d43f0cbea0efbff5aac7a34980afb7
Hesses <romainwanner1998@gmail.com>
2019-05-13 20:42:50 +0200

    Mise à jour de l'interface du bulletin

--- module/bult/bult.php
+++ module/bult/bult.php
@@ -3,18 +3,18 @@
 <table class="table table-hover">
   <thead>
     <tr>
-      <th scope="col">Critères d’évaluation</th>
-      <th scope="col">trésinsuff</th>
-      <th scope="col">insuff</th>
-      <th scope="col">bien</th>
-	  <th scope="col">trésbien</th>
+      <th scope="col"><h3>Critères d’évaluation</h3></th>
+      <th scope="col">Très insuffisant</th>
+      <th scope="col">Insuffisant</th>
+      <th scope="col">Bien</th>
+	  <th scope="col">Très bien</th>
     </tr>
   </thead>
   <tbody>
   	<tr>
 		<td>
 			<center>
-			<h4>Entretien centré sur le parcours de professionnalisation</h4>
+			<h5>Entretien centré sur le parcours de professionnalisation</h5>
 			</center>
 		</td>
 	</tr>
@@ -26,19 +26,127 @@
       <td><input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" ></td>
     </tr>
 	<tr>
+	  <td>Pertinence de l’analyse critique du parcours de professionnalisation</td>
+	  <td><input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" ></td>
+      <td><input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" ></td>
+      <td><input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" ></td>
+      <td><input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" ></td>
+    </tr>
+	<tr>
+	  <td>Variété des situations professionnelles rencontrées</td>
+	  <td><input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" ></td>
+      <td><input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" ></td>
+      <td><input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" ></td>
+      <td><input class="form-check-input" type="radio" name="exampleRadios" id="exampleRadios1" value="option1" ></td>
+    </tr>
+	
+	
+	
+	
+	
+	
+	<tr>
 		<td>
 			<center>
-			<h4>Degré de maîtrise des compétences évaluées relevant de la participation à un projet, de l’accompagnement de la mise en place d’un nouveau service et de l’exploitation d’un service</h4>
+			<h5>Degré de maîtrise des compétences évaluées relevant de la participation à un projet, de l’accompagnement de la mise en place d’un nouveau service et de l’exploitation d’un service</h5>
+			</center>
+		</td>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+	
+	
+	
+	
+	
+	<tr>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td>
+			<center>
+			<h6>Note /10</h6>
 			</center>
 		</td>
 	</tr>
+
+
+
+
+  <tbody>
+  	<tr>
+		<td>
+			<center>
+			<h5>Entretien sur les compétences relevant du processus de gestion du patrimoine informatique</h5>
+			</center>
+		</td>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
     <tr>
-	  <td>Qualité de la présentation</td>
+	  <td>Pertinence d’usage de la gestion des configurations</td>
 	  <td><input class="form-check-input" type="radio" name="exampleRadios1" id="exampleRadios1" value="option1" ></td>
       <td><input class="form-check-input" type="radio" name="exampleRadios1" id="exampleRadios1" value="option1" ></td>
       <td><input class="form-check-input" type="radio" name="exampleRadios1" id="exampleRadios1" value="option1" ></td>
       <td><input class="form-check-input" type="radio" name="exampleRadios1" id="exampleRadios1" value="option1" ></td>
     </tr>
+	<tr>
+	  <td>Efficacité de l’organisation de la veille technologique au service de la construction des compétences</td>
+	  <td><input class="form-check-input" type="radio" name="exampleRadios1" id="exampleRadios1" value="option1" ></td>
+      <td><input class="form-check-input" type="radio" name="exampleRadios1" id="exampleRadios1" value="option1" ></td>
+      <td><input class="form-check-input" type="radio" name="exampleRadios1" id="exampleRadios1" value="option1" ></td>
+      <td><input class="form-check-input" type="radio" name="exampleRadios1" id="exampleRadios1" value="option1" ></td>
+    </tr>
+	
+	
+	
+	
+	<tr>
+		<td>
+			<center>
+			<h5>Degré de maîtrise des compétences évaluées relevant du processus de gestion du patrimoine informatique</h5>
+			</center>
+		</td>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+	<tr>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+	</tr>
+	
+	
+	
+	
+	
+	<tr>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td></td>
+		<td>
+			<center>
+			<h6>Note /10</h6>
+			</center>
+		</td>
+	</tr>
   </tbody>
   
 </table>
