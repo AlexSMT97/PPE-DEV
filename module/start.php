0267efe630a015a9864fa94c63db9f97cbf8c872
Alex <48321321+PEPOZZZZ@users.noreply.github.com>
2019-05-14 14:32:21 +0200

    Update start.php
    update texte 2

--- module/start.php
+++ module/start.php
@@ -4,7 +4,7 @@
   <div class="jumbotron">
     <div class="container">
       <h1 class="display-3">Ouesh le 100!</h1>
-      <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+      <p>Ricardo Milos (Japanese: ロナウ自慰ニョ, Ro nau jii nyo) is a former Brazilian escort notable for his role in Jockbutt, a movie that was created by Mark Wolff Productions and directed by Mark Wolff himself in 2008.</p>
       <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more »</a></p>
     </div>
   </div>
@@ -19,12 +19,12 @@
       </div>
       <div class="col-md-4">
         <h2>Heading</h2>
-        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+        <p>Ricardo Milos was born on November 11, 1977, along with his twin brother, in Rio, Brazil. However, he and his brother are adopted, with a Greek father and a Canadian mother. Ricardo is 5 foot and 8 inches tall and weighs 190 pounds. Ricardo has been modeling for over 10 years and his father is a professional photographer who taught him how to take good photos as well as how to pose in photos. </p>
         <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
       </div>
       <div class="col-md-4">
         <h2>Heading</h2>
-        <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+        <p>Ricardo Milos was born on November 11, 1977, along with his twin brother, in Rio, Brazil. However, he and his brother are adopted, with a Greek father and a Canadian mother. Ricardo is 5 foot and 8 inches tall and weighs 190 pounds. Ricardo has been modeling for over 10 years and his father is a professional photographer who taught him how to take good photos as well as how to pose in photos.</p>
         <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
       </div>
     </div>
