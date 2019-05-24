e8bb3f1f7666c86c2e742f396ebc3a8073078bbc
Alex <cdf.schmitt.alexandre@gmail.com>
2019-05-24 09:08:45 +0200

    Update connexion.php
    Page connexion origine

--- module/connexion/connexion.php
+++ module/connexion/connexion.php
@@ -1,37 +1,3 @@
-<?php
-
-$bdd = new PDO('mysql:host=127.0.0.1;dbname=test-ppe', 'root', '');
-
-    session_start();
-
-    if(!isset($_SESSION["logged"]))
-    {
-        //recherche du joueur dans la liste
-        try
-        {
-            $trouve = false;
-            $req = $bdd->query('SELECT * FROM players');
-            while ($donnees = $req->fetch())
-            {
-                if($donnees['nickname'] == $_POST['pseudo'] && $donnees['password'] == $_POST['mdp'])
-                {
-                    $trouve = true;
-                    $_SESSION["logged"] = $_POST['pseudo'];
-                }
-            }
-
-            if(!$trouve)
-            {
-                header("Location:index.php");
-            }
-        }
-        catch (Exception $e)
-        {
-                die('Erreur : ' . $e->getMessage());
-        }
-    }
-    echo "Bienvenue ".$_SESSION["logged"].", vous êtes connecté !";
-?>
 <link rel="stylesheet" href="./kernel/css/connexion.css">
 <form class="form-signin" action="index.php" method="post">
   <div class="text-center mb-4">
