fb5edd6643fb7ad64e9d8542ef2577d88ab6682a
Hesses <romainwanner1998@gmail.com>
2019-06-20 20:16:59 +0200

    Update co.php

--- co.php
+++ co.php
@@ -6,16 +6,47 @@ if(!isset($_SESSION["logged"]))
 		$trouve = false;
 		$bdd = new PDO("mysql:host=sql27.main-hosting.eu;dbname=u907465831_gsb", "u907465831_gsb", "I8T9Qf58Vh2m");
 		$req = $bdd->query('SELECT * FROM prof');
-		while ($donnees = $req->fetch())
-		{
-			if($donnees['prenom'] == $_POST['prenom'] && $donnees['motdepasse'] == $_POST['mdp'] && $donnees['mail'] == $_POST['email'])
-			{
-				$trouve = true;
-				$_SESSION["logged"] = $_POST['prenom'];
+		
+		
+		$check_prenom = $site->query("SELECT * FROM prof WHERE prenom='". $_POST['prenom'] ."'");
+		$check_prenom->setFetchMode(PDO::FETCH_OBJ);
+		$check_prenom = $check_prenom->fetch();
+		if($check_prenom->$_POST['prenom']== NULL)
+			{								
+				$check_email = $site->query("SELECT * FROM prof WHERE mail='". $_POST['email'] ."'");
+				$check_email->setFetchMode(PDO::FETCH_OBJ);
+				$check_email = $check_email->fetch();
+				if($check_email->$_POST['email']== NULL)
+					{								
+						$check_mdp = $site->query("SELECT * FROM prof WHERE motdepasse='". $_POST['mdp'] ."'");
+						$check_mdp->setFetchMode(PDO::FETCH_OBJ);
+						$check_mdp = $check_mdp->fetch();
+						if($check_mdp->$_POST['mdp']== NULL)
+							{								
+								echo '<div class="alert alert-success" role="alert">
+									 cest bon
+									</div>';
+							}
+						else {
+							echo'
+							<div class="alert alert-danger" role="alert">
+							  Maivaise mots de passe
+							</div>';
+						}
+					}
+				else {
+					echo'
+					<div class="alert alert-danger" role="alert">
+					  Maivaise email
+					</div>';
+				}
 			}
+		else {
+			echo'
+			<div class="alert alert-danger" role="alert">
+			  Mauvais prenom
+			</div>';
 		}
-
-
 }
 ?>
 <link rel="stylesheet" href="./kernel/css/connexion.css">
