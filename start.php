ba58f10f9be79aead4445b51849eefe33a93ce9d
Alex <cdf.schmitt.alexandre@gmail.com>
2019-06-20 21:25:16 +0200

    Update img, start
    Ajout photo, modification texte

--- start.php
+++ start.php
@@ -5,7 +5,7 @@
     <div class="container">
       <h1 class="display-3">Bonjour  <?php echo "".$_SESSION["logged"]."";?> </h1>
       <p>Ce site est développer par Schmitt Alexandre.</p>
-      <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more »</a></p>
+      <p><a class="btn btn-primary btn-lg" href="https://www.20minutes.fr/" role="button">Actualité</a></p>
     </div>
   </div>
 
@@ -13,19 +13,21 @@
     <!-- Example row of columns -->
     <div class="row">
       <div class="col-md-4">
-        <h2>Heading</h2>
-        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-        <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+      <img src="./kernel/img/jul.jpg" width="70" height="90">
+        <h2>Alexandre</h2>
+        <p>Etudiant en BTS SIO option SLAM, Développer principal du site PPE-SIO </p>
+        <p><a class="btn btn-secondary" href="#" role="button">Biographie »</a></p>
       </div>
       <div class="col-md-4">
-        <h2>Heading</h2>
-        <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-        <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+        <h2>Guillaume</h2>
+        <p>Etudiant en BTS SIO option SLAM. </p>
+        <p><a class="btn btn-secondary" href="#" role="button">Biographie »</a></p>
       </div>
       <div class="col-md-4">
-        <h2>Heading</h2>
-        <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-        <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+      <img src="./kernel/img/bioclement.png" width="60" height="60">
+        <h2>Clément</h2>
+        <p>Etudiant en BTS SIO option SLAM.</p>
+        <p><a class="btn btn-secondary" href="#" role="button">Biographie »</a></p>
       </div>
     </div>
 
