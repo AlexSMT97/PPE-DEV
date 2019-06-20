3aac839a550487d1ae56c83d928f23c58b3f65da
Hesses <romainwanner1998@gmail.com>
2019-06-20 20:47:46 +0200

    update

--- co.php
+++ co.php
@@ -4,7 +4,8 @@
 		$trouve = false;
 		$bdd = new PDO("mysql:host=sql27.main-hosting.eu;dbname=u907465831_gsb", "u907465831_gsb", "I8T9Qf58Vh2m");
 		
-		
+	if ($_SESSION != true)
+	{
 		$check_prenom = $bdd->query("SELECT * FROM prof WHERE prenom='". $_POST['prenom'] ."'");
 		$check_prenom->setFetchMode(PDO::FETCH_OBJ);
 		$check_prenom = $check_prenom->fetch();
@@ -32,6 +33,7 @@
 							<div class="alert alert-danger" role="alert">
 							  Maivaise mots de passe
 							</div>';
+							include ('kernel/form_co.php');
 						}
 					}
 				else {
@@ -39,6 +41,7 @@
 					<div class="alert alert-danger" role="alert">
 					  Maivaise email
 					</div>';
+					include ('kernel/form_co.php');
 				}
 			}
 		else {
@@ -46,29 +49,11 @@
 			<div class="alert alert-danger" role="alert">
 			  Mauvais prenom
 			</div>';
+			include ('kernel/form_co.php');
 		}
+	}
+	else
+	{
+		include ('kernel/form_co.php');
+	}
 ?>
-<link rel="stylesheet" href="./kernel/css/connexion.css">
-<form class="form-signin" action="index.php?page=connexion" method="post">
-  <div class="text-center mb-4">
-    <img class="mb-4" src="/docs/4.3/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
-    <h1 class="h3 mb-3 font-weight-normal">Connexion</h1>
-    <p></p>
-  </div>
-
-  <div class="form-label-group">
-    <input type="text" name="prenom" id="prenom" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="">
-    <label for="inputEmail">Prénom</label>
-  </div>
-
-  <div class="form-label-group">
-    <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="">
-    <label for="inputEmail">Email</label>
-  </div>
-
-  <div class="form-label-group">
-    <input type="password" name="mdp" id="inputPassword" class="form-control" placeholder="Password" required="">
-    <label for="inputPassword">Mots de passe</label>
-  </div>
-  <button class="btn btn-lg btn-primary btn-block" type="submit" name='submit'>Connexion</button>
-</form>
